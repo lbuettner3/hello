9d1adc31379b0fa95268ebf9ae29d9d550ba06de
lbuettner3 <lbuettner3@huskers.unl.edu>
2021-08-29 02:59:43 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Luke Buettner
+ * Date: 8/24/21
+ *
+ * A Simple hello world program.
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Luke Buettner\n");
+
+     return 0;
+
+ }
